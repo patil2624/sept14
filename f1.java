e140c3eb8fb4565dfebbd2997b4cb5b134f560c0
patil2624 <ukumar01292@gmail.com>
2024-09-16 11:49:59 +0530

    Update f1.java

--- f1.java
+++ f1.java
@@ -1,22 +1,61 @@
+import java.util.*;
 
-import java.util.Scanner;
+// Java code for printing pattern
+public class GeeksForGeeks {
 
-class Main {
-  public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
+    // Function to demonstrate pattern
+    public static void printPattern(int n)
+    {
+        int i, j;
+        int num = 1;
 
-    System.out.println("Enter name, age and salary:");
+        // outer loop to handle upper part
+        for (i = 1; i <= n; i++) {
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
 
-    // String input
-    String name = myObj.nextLine();
+            // inner loop to print spaces
+            int spaces = 2 * (n - i);
+            for (j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
 
-    // Numerical input
-    int age = myObj.nextInt();
-    double salary = myObj.nextDouble();
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
 
-    // Output input by user
-    System.out.println("Name: " + name);
-    System.out.println("Age: " + age);
-    System.out.println("Salary: " + salary);
-  }
+            System.out.println();
+        }
+
+        // outer loop to handle lower part
+        for (i = n; i >= 1; i--) {
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // inner loop to print spaces
+            int spaces = 2 * (n - i);
+            for (j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    // Driver Function
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
 }
